38b7b68fd0a83ca0d667503a1c64ae0ea0721dcf
PC-Reviver <->
2019-06-03 13:04:47 +0200

    Correct contact tussen components

--- src/app/leningen/lening.component.ts
+++ src/app/leningen/lening.component.ts
@@ -13,12 +13,12 @@ export class LeningComponent {
   constructor() {
     /* Tijdelijke manier om leningen te vullen,
      * Moet uiteindelijk vanuit de database. */
-    this.leningen = this.maakLeningen();
+    this.leningen = this.getLeningen();
     console.log("LeningComponent constructor");
   }
 
   // Tijdelijke methode
-  public maakLeningen(): Lening[] {
+  public getLeningen(): Lening[] {
     let l1: Lening = new Lening(1, 100001, new Date("2019-4-10"), new Date("2019-8-10"));
     let l2: Lening = new Lening(2, 100001, new Date("2019-4-11"), new Date("2019-8-11"));
     let l3: Lening = new Lening(3, 100002, new Date("2019-4-11"), new Date("2020-8-11"));
@@ -27,20 +27,20 @@ export class LeningComponent {
   }
 
   public formatDate(date: Date): string {
-    let result: string = '';
+    let result: string = "";
     if (date != null) {
-      //console.log("formatdate");
-      let day: string = '' + date.getDate();
-      let month: string = '' + date.getMonth() + 1;
-      let year: string = '' + date.getFullYear();
-      // Om dag en maand altijd 2 characters lang te maken, bijv. "05".
+      //console.log("LeningComponent formatDate");
+      let day: string = "" + date.getDate();
+      let month: string = "" + date.getMonth() + 1;
+      let year: string = "" + date.getFullYear();
+      // Maakt dag en maand altijd 2 characters lang, bijv. "05".
       if (day.length < 2) {
-        day = '0' + day;
+        day = "0" + day;
       }
       if (day.length < 2) {
-        month = '0' + month;
+        month = "0" + month;
       }
-      result = day + '-' + month + '-' + year;
+      result = day + "-" + month + "-" + year;
     }
     return result;
   }
--- src/app/leningen/stlening.component.ts
+++ src/app/leningen/stlening.component.ts
@@ -1,59 +1,39 @@
 import { Component } from '@angular/core';
 import { Lening } from '../model/lening';
+import { LeningComponent } from './lening.component';
 
 @Component({
+  providers: [LeningComponent],
   selector: 'stLeningen',
   templateUrl: './stlening.component.html',
   styleUrls: ['../app.component.css']
 })
 
 export class StLeningComponent {
-  leningen: Lening[];
   stLeningen: Lening[];
+  /* Tijdelijke manier om studentNr te geven,
+   * Moet uiteindelijk vanuit de UI. */
+  studentNr: number = 100001;
 
-  constructor() {
-    /* Tijdelijke manier om leningen te vullen,
-     * Moet uiteindelijk vanuit de database. */
-    this.leningen = this.maakLeningen();
-    this.stLeningen = [];
+  constructor(private lcomp: LeningComponent) {
+    this.stLeningen = this.getStudentsLeningen(this.studentNr);
     console.log("StLeningComponent constructor");
   }
 
-  // Tijdelijke methode
-  public maakLeningen(): Lening[] {
-    let l1: Lening = new Lening(1, 100001, new Date("2019-4-10"), new Date("2019-8-10"));
-    let l2: Lening = new Lening(2, 100001, new Date("2019-4-11"), new Date("2019-8-11"));
-    let l3: Lening = new Lening(3, 100002, new Date("2019-4-11"), new Date("2020-8-11"));
-    let ls: Lening[] = [l1, l2, l3];
-    return ls;
-  }
-
-  public vulStudentsLeningen(studentNr: number) {
+  public getStudentsLeningen(studentNr: number): Lening[] {
     let result: Lening[] = [];
-    for (let l of this.leningen) {
-      if (l.studentNr == studentNr) {
-        result[result.length] = l;
+    let leningen: Lening[] = this.lcomp.getLeningen();
+    //if (leningen != null && leningen.length > 0) {
+      for (let lening of leningen) {
+        if (lening.studentNr == studentNr) {
+          result[result.length] = lening;
+        }
       }
-    }
-    this.stLeningen = result;
+    //}
+    return result;
   }
 
   public formatDate(date: Date): string {
-    let result: string = '';
-    if (date != null) {
-      //console.log("formatdate");
-      let day: string = '' + date.getDate();
-      let month: string = '' + date.getMonth() + 1;
-      let year: string = '' + date.getFullYear();
-      // Om dag en maand altijd 2 characters lang te maken, bijv. "05".
-      if (day.length < 2) {
-        day = '0' + day;
-      }
-      if (day.length < 2) {
-        month = '0' + month;
-      }
-      result = day + '-' + month + '-' + year;
-    }
-    return result;
+    return this.lcomp.formatDate(date);
   }
 }
