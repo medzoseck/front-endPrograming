c7c462747daeb1b630d96246f49afcdf6cf281a0
Jotte Smit <jotte_smit@hotmail.com>
2019-06-09 00:20:35 +0200

    Volledige CRUD op inventaris.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from  '../auth/auth.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private  authService:  AuthService) { }
+  constructor(private authService: AuthService) { }
   ngOnInit() {}
 
 }
---
+++ src/app/product-detail/product-detail.resolver.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot} from '@angular/router';
+import { FirebaseService } from '../services/firebase.service';
+
+@Injectable()
+export class ProductDetailResolver implements Resolve<any> {
+
+    constructor(public firebaseService: FirebaseService) { }
+
+    resolve(route: ActivatedRouteSnapshot) {
+
+        return new Promise((resolve) => {
+            const productenId = route.paramMap.get('id');
+            this.firebaseService.getProduct(productenId)
+                .subscribe(
+                    data => {
+                        resolve(data);
+                    }
+                );
+        });
+    }
+}
