ecf886c5e1eb7e3f39f43c2973e90f54d4f0a5c1
medzoseck <medzoseck@hotmail.com>
2019-06-09 18:42:25 +0200

    user service fix

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,8 +5,11 @@ import { AngularFirestore } from '@angular/fire/firestore';
   providedIn: 'root'
 })
 export class UserService implements OnInit {
+    user : User;
 
-    constructor(public db: AngularFirestore) {}
+    constructor(public db: AngularFirestore) {
+        this.user = new User();
+    }
     
     ngOnInit() {
     }
@@ -16,7 +19,36 @@ export class UserService implements OnInit {
         return gebruikerID;
     }
     // haalt user op uit de database die de gebruiker is    
-    getUser(userKey) {
-        return this.db.collection('Users').doc('userKey').snapshotChanges();
+    getUser() : User {
+       
+        this.db.collection('Users').snapshotChanges().subscribe( item => {
+            item.forEach(element => {
+               if(element.payload.doc.get("gebruiker") == this.GetGebruikerID()){
+                this.user.admin = element.payload.doc.get("admin");
+                this.user.gebruiker = element.payload.doc.get("gebruiker");
+                this.user.studentnr = element.payload.doc.get("studentnr");
+               }
+            })
+        });
+        return this.user;
     }
+    // checkt de rol van de huidige gebruiker
+    isAdmin() : boolean {
+        this.db.collection('Users').snapshotChanges().subscribe( item => {
+            item.forEach(element => {
+                if(element.payload.doc.get("gebruiker") == this.GetGebruikerID()){
+                    if(element.payload.doc.get("admin") == true){
+                        return true;
+                    }      
+                }
+            })
+        });
+        return false;
+    }
+}
+
+class User {
+    gebruiker: string;
+    studentnr: string;
+    admin: boolean;
 }
